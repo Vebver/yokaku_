7d96ede31a8e401123217329dd9046e1a70a1ce9
CLProg <carllorenz32@gmail.com>
2026-05-03 02:24:17 +0800

    lastUpdate

--- frontend/src/components/FeaturedMenu.jsx
+++ frontend/src/components/FeaturedMenu.jsx
@@ -6,8 +6,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../Style/FeaturedMenu.css";
 
-const API_BASE = "https://yokaku-backend.onrender.com/api";
-const BASE_URL = "https://yokaku-backend.onrender.com";
+const API_BASE = import.meta.env.VITE_API_URL;
+const BASE_URL = import.meta.env.VITE_SOCKET_URL;
 
 function FeaturedMenu() {
   const [featuredItems, setFeaturedItems] = useState([]);
