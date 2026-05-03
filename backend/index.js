25d39c3cb89970c86f7e9b6168af2e622dc5c15b
V <johnpanganiban383@gmail.com>
2026-05-03 15:31:10 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ const io = new Server(server, {
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:5173",
-      "https://yokaku-drab.vercel.app",
+      "https://yokaku-tau.vercel.app",
     ],
     methods: ["GET", "POST"],
     credentials: true,
@@ -95,7 +95,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:5173",
-      "https://yokaku-drab.vercel.app",
+      "https://yokaku-tau.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
